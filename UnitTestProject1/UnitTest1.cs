9496da82b771198839710d5f1b91056de8d33fd1
alena <alena@KOLLAPS>
2023-04-03 19:19:07 +0300

    Добавьте файлы проекта.

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,82 @@
+﻿using BankAccountNS;
+using BankAccountWpf;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class BankAccountTests
+    {
+        [TestMethod]
+        public void Credit_WithValidAmount_UpdatesBalance()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double creditAmount = 5.77;
+            double expected = 17.76;
+            BankAccount account = new BankAccount("Mr.Roman Abramovich", beginningBalance);
+            // Act
+            account.Credit(creditAmount);
+
+            // Assert
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Account not credited correctly");
+        }
+
+        [TestMethod]
+        public void Debit_WithValidAmount_UpdatesBalance()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 4.55;
+            double expected = 7.44;
+            BankAccount account = new BankAccount("Mr.Roman Abramovich", beginningBalance);
+
+            // Act
+            account.Debit(debitAmount);
+
+            // Assert
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+        }
+
+        [TestMethod]
+        public void Debit_WithAmountExceedsBalance_ThrowsException()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 20.00;
+            BankAccount account = new BankAccount("Mr.Roman Abramovich", beginningBalance);
+
+            // Act & Assert
+            Assert.ThrowsException<System.ArgumentOutOfRangeException>(() => account.Debit(debitAmount));
+        }
+
+        [TestMethod]
+        public void Debit_WithAmountLessThanZero_ThrowsException()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = -5.77;
+            BankAccount account = new BankAccount("Mr.Roman Abramovich", beginningBalance);
+
+            // Act & Assert
+            Assert.ThrowsException<System.ArgumentOutOfRangeException>(() => account.Debit(debitAmount));
+        }
+
+        [TestMethod]
+        public void Credit_WithAmountLessThanZero_ThrowsException()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double creditAmount = -5.77;
+            BankAccount account = new BankAccount("Mr.Roman Abramovich", beginningBalance);
+
+            // Act & Assert
+            Assert.ThrowsException<System.ArgumentOutOfRangeException>(() => account.Credit(creditAmount));
+        }
+
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Windows;
+
+namespace BankAccountWpf
+{
+    public partial class MainWindow : Window
+    {
+        private BankAccountNS.BankAccount account;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            account = new BankAccountNS.BankAccount("Mr.Roman Abramovich", 11.99);
+
+            DataContext = account;
+        }
+
+        private void creditButton_Click(object sender, RoutedEventArgs e)
+        {
+            double amount;
+            if (double.TryParse(amountTextBox.Text, out amount))
+            {
+                try
+                {
+                    account.Credit(amount);
+                    balanceTextBlock.Text = account.Balance.ToString("C");
+                }
+                catch (System.ArgumentOutOfRangeException ex)
+                {
+                    MessageBox.Show(ex.Message, "Error");
+                }
+            }
+        }
+        private void debitButton_Click(object sender, RoutedEventArgs e)
+        {
+            double amount;
+            if (double.TryParse(amountTextBox.Text, out amount))
+            {
+                try
+                {
+                    account.Debit(amount);
+                    balanceTextBlock.Text = account.Balance.ToString("C");
+                }
+                catch (System.ArgumentOutOfRangeException ex)
+                {
+                    MessageBox.Show(ex.Message, "Error");
+                }
+            }
+        }
+    }
+}
